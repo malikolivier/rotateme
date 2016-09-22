4b8f787b905fd0534a074ee8f940b4bfb7c4cc0a
Malik Olivier Boussejra <malik@boussejra.com>
2016-09-23 04:45:30 +0900

    Got recording working

--- public/javascripts/render.js
+++ public/javascripts/render.js
@@ -1,8 +1,9 @@
-define(['three', 'jquery', './OrbitControls'], function(THREE, $) {
+define(['three', 'jquery', './whammy', './OrbitControls'], function(THREE, $, Whammy) {
     var Render = {
         debugging: false,
         // shaderLoaded: function called back when shaders are finished loading
         start: function(shaderLoaded) {
+            this.measure = "off";
             var self = this;
             var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.01, 10e20);
             var scene = new THREE.Scene();
@@ -101,6 +102,42 @@ define(['three', 'jquery', './OrbitControls'], function(THREE, $) {
             };
             trySetCanvasSize();
 
+            function dataURItoBlob(dataURI) {
+                var mimetype = dataURI.split(",")[0].split(':')[1].split(';')[0];
+                var byteString = atob(dataURI.split(',')[1]);
+                var u8a = new Uint8Array(byteString.length);
+                for (var i = 0; i < byteString.length; i++) {
+                    u8a[i] = byteString.charCodeAt(i);
+                }
+                return new Blob([u8a.buffer], {
+                    type: mimetype
+                });
+            }
+
+            function saveSnapshot(blob, type, done) {
+                if (typeof cordova !== 'undefined') {
+                    // Save to phone memory
+                    var now = new Date();
+                    var filename = now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate() +
+                        "_" + now.getHours() + "-" + now.getMinutes() + "-" + now.getSeconds();
+                    var file = new app.io.File("snapshot-" + filename + "." + type, app.io.PICTURES, function(file) {
+                        file.write(blob, function() {
+                            // Do something when the write has finished
+                            //window.location.assign(file.getPath()); // DIRTY TEST to show the picture in the WebView
+                            if (done) done();
+                        });
+                    });
+                }
+            }
+
+            var takeSnapshot = false,
+                imgData;
+            var takeVideoSnapshot = false,
+                stopVideoSnapshot = false,
+                encoder = null,
+                frameCounter = 0;
+            var autoRotateSpeed = 0;
+
             function render() {
                 // Render background
                 cameraCube.rotation.copy(camera.rotation);
@@ -114,6 +151,58 @@ define(['three', 'jquery', './OrbitControls'], function(THREE, $) {
                 controls.update();
 
                 renderer.render(scene, camera);
+                $.each(moreScenes, function(i, val) {
+                    renderer.clearDepth();
+                    renderer.render(val.scene, val.camera);
+                });
+
+                if (takeSnapshot) {
+                    takeSnapshot = false;
+                    imgData = renderer.domElement.toDataURL();
+                    var imgBlob = dataURItoBlob(imgData);
+                    saveSnapshot(imgBlob, "png");
+                    var anchor = document.createElement('a');
+                    anchor.href = imgData;
+                    var fileName = "snapshot.png";
+                    anchor.setAttribute('download', fileName);
+                    anchor.setAttribute('target', '_blank');
+                    // The element must be appended to the DOM for it to work on Firefox
+                    document.body.appendChild(anchor);
+                    anchor.click();
+                }
+                if (takeVideoSnapshot) {
+                    if (encoder === null) {
+                        encoder = new Whammy.Video(15);
+                    }
+                    if (frameCounter % 4 === 0) {
+                        imgData = renderer.domElement.toDataURL("image/webp");
+                        encoder.add(imgData);
+                        var gl = renderer.getContext();
+                        var pixels = new Uint8Array(gl.drawingBufferWidth * gl.drawingBufferHeight * 4);
+                        gl.readPixels(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+                    }
+                    frameCounter++;
+                    if (stopVideoSnapshot) {
+                        takeVideoSnapshot = false;
+                        stopVideoSnapshot = false;
+                        var output = encoder.compile();
+                        encoder = null;
+                        saveSnapshot(output, "webm", function() {
+                            alert("Snapshot saved to storage!");
+                        });
+                        var url = window.URL.createObjectURL(output);
+                        var anchor = document.createElement('a');
+                        anchor.href = url;
+                        var fileName = "snapshot.webm";
+                        anchor.setAttribute('download', fileName);
+                        anchor.setAttribute('target', '_blank');
+                        // The element must be appended to the DOM for it to work on Firefox
+                        document.body.appendChild(anchor);
+                        anchor.click();
+                    }
+                }
+                Render.moveMesh(autoRotateSpeed);
+
                 requestAnimationFrame(render);
             }
 
@@ -170,6 +259,26 @@ define(['three', 'jquery', './OrbitControls'], function(THREE, $) {
                 // Must reset the rotation of the mesh as well
                 this.getMesh().rotation.y = 0;
             };
+            var moreScenes = [];
+            this.addScene = function(scene, camera) {
+                moreScenes.push({
+                    scene: scene,
+                    camera: camera
+                });
+            };
+            this.takeSnapshot = function() {
+                takeSnapshot = true;
+            };
+            this.takeVideoSnapshot = function() {
+                if (takeVideoSnapshot) {
+                    stopVideoSnapshot = true;
+                } else {
+                    takeVideoSnapshot = true;
+                }
+            };
+            this.setAutoRotateSpeed = function(speed) {
+                autoRotateSpeed = speed;
+            };
 
         },
 
@@ -203,16 +312,31 @@ define(['three', 'jquery', './OrbitControls'], function(THREE, $) {
                         throw "Unkown texture type as argument: " + textures;
                     }
                     humanMesh.name = "human";
+                    // Set material to DoubleSide to enable raycasting from within the mesh
+                    humanMesh.material.side = THREE.DoubleSide;
                     var scene = Render.getScene();
                     scene.remove(scene.getObjectByName("human"));
                     scene.add(humanMesh);
                 });
             };
-        })()
+        })(),
+
+        moveMesh: function(direction) {
+            var mesh = Render.getMesh()
+            if (mesh) {
+                mesh.rotation.y += Math.PI / 2 * direction;
+            }
+        }
+
     };
 
     // Pollute global namespace:
     window.changeBG = Render.changeBG;
+    setTimeout( function() {
+        window.takeSnapshot = Render.takeSnapshot;
+        window.takeVideoSnapshot = Render.takeVideoSnapshot;
+        window.setAutoRotateSpeed = Render.setAutoRotateSpeed;
+    }, 1000);
 
     return Render;
 });
